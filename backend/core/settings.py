7d26c9165f4afbd95424476f90c3b94f24378c68
Hồ Khánh Duy <22120076@student.hcmus.edu.vn>
2024-02-12 15:50:21 +0700

    fix bug nhung ko co bug

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -80,10 +80,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        "NAME": "test",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "localhost",
+        "NAME": os.environ.get('DB_NAME'),
+        "USER": os.environ.get('DB_USER'),
+        "PASSWORD": os.environ.get('DB_PASS'),
+        "HOST": os.environ.get('DB_HOST'),
+        "PORT": os.environ.get('DB_PORT','3306'),
     }
 }
 
